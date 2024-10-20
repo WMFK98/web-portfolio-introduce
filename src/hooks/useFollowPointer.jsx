db6c3e79cdbf4b501a1fdd9150ef7f16c4ee8468
realFK98 <wishira2013.coth@gmail.com>
2024-10-20 21:12:56 +0700

    up scale

--- src/hooks/useFollowPointer.jsx
+++ src/hooks/useFollowPointer.jsx
@@ -1,7 +1,7 @@
 import { useState, RefObject, useEffect } from "react";
 import { useMotionValue, useSpring, frame } from "framer-motion";
 
-const spring = { damping: 20, stiffness: 50, restDelta: 0.001 };
+const spring = { damping: 40, stiffness: 50, restDelta: 0.001 };
 
 export function useFollowPointer(ref) {
   const xPoint = useMotionValue(0);
